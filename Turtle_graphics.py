b5433a0aaa966bb1e430b2dcfe02f99e329230c1
Sathish B Balasubramaniam <sathishbbala@gmail.com>
2025-11-10 15:15:21 +0530

    commit #2

--- Turtle_graphics.py
+++ Turtle_graphics.py
@@ -2,7 +2,10 @@ from turtle import Turtle, Screen
 import random
 colors = ["DarkOrchid", "DeepSkyBlue", "Gold", "LimeGreen", "Tomato", "OrangeRed", "MediumVioletRed", "Turquoise"]
 tim = Turtle()
-
+tim.pensize(2)
+tim.speed('fastest')
+timmy = Turtle()
+tommy = Turtle()
 def draw_shape(number_of_sides):
     angle = 360 / number_of_sides
     for _ in range(number_of_sides):
@@ -12,13 +15,63 @@ def draw_shape(number_of_sides):
 def random_walk(steps):
     directions = [0, 90, 180, 270]
     tim.pensize(10)
-    tim.speed('fastest')
     tim.color(random.choice(colors))
     tim.setheading(random.choice(directions))
     tim.forward(steps)  
 
-for i in range(200):
-    random_walk(30)   
+# this did not work need to check why it is not working 
+# using the colors list to choose a random color
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_spirograph(num_circles, size_of_gap):
+    for _ in range(int(num_circles)):
+        tim.color(random.choice(colors))
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+def move_forward():
+    tim.forward(10) 
+
+def move_backward():
+    tim.backward(10)
+
+def turn_right():
+    tim.right(10)
+
+def turn_left():
+    tim.left(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home() 
+    tim.pendown()
+# Example usages: the example below to draw a spirograph
+#draw_spirograph(100,10)
+
+timmy.color("blue")
+timmy.shape("turtle")
+
+tommy.color("red")
+tommy.shape("turtle")   
+
+
+screen = Screen()
+#screen.listen()
+#screen.onkey(key = "W", fun = move_forward)
+#screen.onkey(key = "S", fun = move_backward)
+#screen.onkey(key = "A", fun = turn_left)
+#screen.onkey(key = "D", fun = turn_right)
+#screen.onkey(key = "C", fun = clear)
+screen.title("Turtle Graphics")
+screen.setup(width=1000, height=600)
+screen.exitonclick()
+
 
 
 
