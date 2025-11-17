93ed9d60c4794a600d38dc243a15f64749ef0407
Sathish B Balasubramaniam <sathishbbala@gmail.com>
2025-11-17 21:04:48 +0530

    Adding pomodoro.py

---
+++ 10-pomodoro.py
@@ -0,0 +1,77 @@
+from tkinter import *
+import math
+
+#  Constants
+RED = "#BF1A1A"
+PINK = "#FF8FB7"
+GREEN = "#3A6F43"
+SHORT_BREAK_SECS = 30
+LONG_BREAK_SECS = 120
+WORK_SECS = 150
+reps = 0 
+timer = None
+
+# Function reset_timer 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text = "00:00")
+    title_label.config(text = "Timer", fg = "#9bdeac")
+    check_mark.config(text = "")
+    global reps
+    reps = 0
+
+# Function start_timer
+def start_timer():
+    global reps
+    reps +=1
+    if reps % 2 == 1:
+        count_down(WORK_SECS)
+        title_label.config(text = "Work", fg = GREEN)
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_SECS)
+        title_label.config(text = "Break", fg = RED)
+    elif reps == 8:
+        count_down(LONG_BREAK_SECS)
+        title_label.config(text = "Break", fg = PINK)
+    
+
+# Function countdown 
+def count_down(count):
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+    canvas.itemconfig(timer_text, text = f"{count_minutes:02d}:{count_seconds:02d}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count -1)    
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_mark.config(text = marks)
+
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(padx=100, pady=50, bg="#FFF8D4")
+
+title_label = Label(text = "Timer", fg = "#9bdeac", bg = "#FFF8D4", font = ("Courier", 50, "bold"))
+title_label.grid(column = 1, row = 0)
+
+canvas = Canvas(width=220, height=224, bg="#FFF8D4", highlightthickness=0)
+tomato_img = PhotoImage(file = "tomato.png")
+canvas.create_image(103, 112, image = tomato_img)
+timer_text = canvas.create_text(103, 130, text="00:00", fill = "white", font=("courier", 35, "bold"))
+canvas.grid(column = 1, row = 1)
+
+start_button = Button(text = "Start", highlightthickness = 0, command = start_timer)
+start_button.grid(column = 0, row = 2)
+
+reset_button = Button(text = "Reset", highlightthickness = 0, command = reset_timer)
+reset_button.grid(column = 2, row = 2)
+
+#check_mark = Label(text = "✓", fg = "#9bdeac", bg = "#FFF8D4")
+check_mark = Label(text = "", fg = "#9bdeac", bg = "#FFF8D4")
+check_mark.grid(column = 1, row = 3)
+
+window.mainloop()
--- 5-tkinter.py
+++ 5-tkinter.py
@@ -1,14 +1,37 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My First Window")
-window.minsize(width=1200, height=600)
+def calculate_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    kilometer_result_label.config(text=f"{km:.2f}")
 
-# Label
-my_label = tkinter.Label(text = "I am a Label", font = ("Arial", 24, "bold"))
-my_label.pack()
+window = Tk()
+window.title("Mile to Kilometer Converter")
+window.config(padx = 20, pady = 20)
 
+# Allow grid expansion
+#for i in range(3):
+#    window.columnconfigure(i, weight=1)
+#for i in range(3):
+#    window.rowconfigure(i, weight=1)
 
+# Widgets
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0, sticky="nsew", pady=10)
 
-# To make the window stay up
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0, sticky="w")
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1, sticky="e")
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1, sticky="nsew")
+
+kilometer_label = Label(text="KMs")
+kilometer_label.grid(column=2, row=1, sticky="w")
+
+calculate_button = Button(text="Calculate", command=calculate_km)
+calculate_button.grid(column=1, row=2, sticky="nsew", pady=20)
+
 window.mainloop()
